61d8eb00525b153ac6563216d01330a71f63add7
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2022-10-09 17:41:50 +0100

    Update Disabler.java

--- src/main/java/dev/rise/module/impl/other/Disabler.java
+++ src/main/java/dev/rise/module/impl/other/Disabler.java
@@ -1,8 +1,3 @@
-/*
- Copyright Alan Wood 2021
- None of this code to be reused without my written permission
- Intellectual Rights owned by Alan Wood
- */
 package dev.rise.module.impl.other;
 
 import dev.rise.Rise;
